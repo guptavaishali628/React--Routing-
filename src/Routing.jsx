0a697a2885e12a816d908004d957f0945231507e
vaishali <guptavaishali628@gmail.com>
2026-01-07 18:28:15 +0530

    Router Dom

---
+++ src/Routing.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+const Routing = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+        {/* bydefault isko dikhane k liye on layout */}
+            <Route index element={<Home/>}></Route> 
+            <Route path='about' element={<About/>}></Route> 
+            <Route  path='contact' element={<Contact/>}></Route> 
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default Routing
